7d5fa71fbe75d12e4d89f683e8375e929d3340e2
Cao Hai Duong <46746869+haiduong004@users.noreply.github.com>
2023-06-13 04:13:52 +0700

    Update account.controller.js

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -1,10 +1,13 @@
 const {Account} = require("../models");
+const randToken = require('rand-token');
+const dotenv = require("dotenv")
+const bcrypt = require('bcrypt');
 
 // const Register = async(req,res) => {
 //     try {
 //         const {userName,fullName,email,password} = req.body;
-//         const newAccount = await Account.create({userName,fullName,email,password});
-//         res.status(201).send(newAccount);
+//         const newUser = await Account.create({userName,fullName,email,password});
+//         res.status(201).send("Success register user account");
 //     } catch (error) {
 //         res.status(500).send(error)
 //     }
@@ -13,8 +16,25 @@ const {Account} = require("../models");
 const Register = async(req,res) => {
     try {
         const {userName,fullName,email,password} = req.body;
-        const newAccount = await Account.create({userName,fullName,email,password});
-        res.status(201).send(newAccount);
+
+        //kiểm tra tài khoản có tồn tại
+        const user = await Account.findOne({ where: { userName } });
+        const hashPassword = bcrypt.hashSync(req.body.password, SALT_ROUNDS = 10);
+        if (!user) {
+            if (userName && password){
+                const newUser = await Account.create({
+                    userName: userName,
+                    fullName: fullName,
+                    email: email,
+                    password: hashPassword});
+                res.status(201).send("Success register user account");
+            } else {
+                res.status(409).send('Invalid input');
+            }
+        }
+        else {
+            res.status(409).send('This username has already existed');
+        }
     } catch (error) {
         res.status(500).send(error)
     }
@@ -34,14 +54,15 @@ const Login = async(req,res) => {
         const {userName,password} = req.body;
         const user = await Account.findOne({ where: { userName } });
         if (!user) {
-            res.status(401).send('Tên đăng nhập không tồn tại.');
+            res.status(401).send('Username does not exist');
         } else {
-            if (user.password == password) {
+            const isPasswordValid = bcrypt.compareSync(password, user.password);
+            if (isPasswordValid) {
                 const fullName = user.fullName;
                 const email = user.email;
                 res.status(200).send({userName,password,fullName,email});
             } else {
-                res.status(401).send('Mật khẩu không chính xác.');
+                res.status(401).send('Incorrect password');
             }
         }
     } catch (error) {
